6d632b845469eecd2c5e810d8588bb6f7464bc5f
hy <hy@gmail.com>
2024-06-26 10:08:00 +0800

    add new page
    Signed-off-by: hy <hy@gmail.com>

--- ChinesePoetryLibrary/ChinesePoetryLibrary/MainWindow.xaml.cs
+++ ChinesePoetryLibrary/ChinesePoetryLibrary/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using ChinesePoetryLibrary.Src;
 using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -13,6 +14,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -35,6 +37,13 @@ namespace ChinesePoetryLibrary
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
 
+            AppWindow.SetIcon("Assets/icon.ico");
+
+            //
+            AppTitleBar.Loaded += AppTitleBar_Loaded;
+            AppTitleBar.SizeChanged += AppTitleBar_SizeChanged;
+            //
+
             NavView.SelectedItem = NavItemAuthor;
             ContentFrame.Navigate(Type.GetType("ChinesePoetryLibrary.Pages.Author"));
 
@@ -51,5 +60,58 @@ namespace ChinesePoetryLibrary
             ContentFrame.Navigate(pageType);
         }
 
+        private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ExtendsContentIntoTitleBar == true)
+            {
+                // Set the initial interactive regions.
+                SetRegionsForCustomTitleBar();
+            }
+        }
+
+        private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ExtendsContentIntoTitleBar == true)
+            {
+                // Update interactive regions if the size of the window changes.
+                SetRegionsForCustomTitleBar();
+            }
+        }
+
+        private void SetRegionsForCustomTitleBar()
+        {
+            double scaleAdjustment = AppTitleBar.XamlRoot.RasterizationScale;
+
+            RightPaddingColumn.Width = new GridLength(AppWindow.TitleBar.RightInset / scaleAdjustment);
+            LeftPaddingColumn.Width = new GridLength(AppWindow.TitleBar.LeftInset / scaleAdjustment);
+
+            GeneralTransform transform = TitleBarSearchBox.TransformToVisual(null);
+            Rect bounds = transform.TransformBounds(new Rect(0, 0,
+                                                             TitleBarSearchBox.ActualWidth,
+                                                             TitleBarSearchBox.ActualHeight));
+            Windows.Graphics.RectInt32 SearchBoxRect = GetRect(bounds, scaleAdjustment);
+
+            transform = PersonPic.TransformToVisual(null);
+            bounds = transform.TransformBounds(new Rect(0, 0,
+                                                        PersonPic.ActualWidth,
+                                                        PersonPic.ActualHeight));
+            Windows.Graphics.RectInt32 PersonPicRect = GetRect(bounds, scaleAdjustment);
+
+            var rectArray = new Windows.Graphics.RectInt32[] { SearchBoxRect, PersonPicRect };
+
+            InputNonClientPointerSource nonClientInputSrc =
+                InputNonClientPointerSource.GetForWindowId(this.AppWindow.Id);
+            nonClientInputSrc.SetRegionRects(NonClientRegionKind.Passthrough, rectArray);
+        }
+
+        private Windows.Graphics.RectInt32 GetRect(Rect bounds, double scale)
+        {
+            return new Windows.Graphics.RectInt32(
+                _X: (int)Math.Round(bounds.X * scale),
+                _Y: (int)Math.Round(bounds.Y * scale),
+                _Width: (int)Math.Round(bounds.Width * scale),
+                _Height: (int)Math.Round(bounds.Height * scale)
+            );
+        }
     }
 }
